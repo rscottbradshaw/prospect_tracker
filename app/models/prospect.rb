24be7ad8756dff5b614b552392290439148c695d
R Scott Bradshaw <scottbradshaw@nc.rr.com>
2015-06-02 21:24:10 -0400

    add dropdown to navigation bar for department, statuses and archive

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -2,8 +2,6 @@ class Prospect < ActiveRecord::Base
   has_many :emails
   belongs_to :department
   belongs_to :status
-  scope :active, -> { where(active: true) }
-  # scope :archive, -> { where(archive: true) }
 
   def self.archive
     self.includes(:status).where(statuses: { name: ["Declined", "Hired"]} )
@@ -12,5 +10,5 @@ class Prospect < ActiveRecord::Base
   def self.active
     Prospect.includes(:status).where.not(statuses: { name: ["Declined", "Hired"]} )
   end
-  
+
 end
